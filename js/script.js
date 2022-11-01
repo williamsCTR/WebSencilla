f0757c3b673604bc36ae709558ee4aef22bed396
williamsCTR <willycantero18@gmail.com>
2022-11-01 14:50:47 -0300

    Se agrega proyecto completo

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//SCROLL REVEAL
+
+window.sr = ScrollReveal();
+sr.reveal(".navbar",{
+    duration:2000,
+    origin:"bottom"
+})
+
+sr.reveal(".header__container--left",{
+    duration:2000,
+    origin:"top",
+    distance:"3em"
+})
+
+sr.reveal(".header__container--right",{
+    duration:2000,
+    origin:"right",
+    distance:"3em"
+})
+
+sr.reveal(".testimonial", {
+    duration: 2000,
+    origin: "left",
+    distance: "3em",
+    viewFactor: 0.2
+  });
+
+  sr.reveal(".info__container--left", {
+    duration: 2000,
+    origin: "left",
+    distance: "3em",
+    viewFactor: 0.2
+  });
+
+  sr.reveal(".info__container--right", {
+    duration: 2000,
+    origin: "right",
+    distance: "3em",
+  });
+
+  sr.reveal(".infoTwo",{
+    duration:2000,
+    origin:"bottom",
+    distance:"2em"
+})
+
+sr.reveal(".contact",{
+    duration:2000,
+    origin:"top",
+    distance:"2em"
+})
+
+sr.reveal(".footer",{
+    duration:1000,
+    origin:"top",
+    distance:"1em"
+})
